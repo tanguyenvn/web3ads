74030012312102a580084f608e8b1262449287d6
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2024-11-16 17:19:17 +0700

    Add WorldID login + cleanups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,65 +2,18 @@
 
 import { useWalletStore } from "@/components/stores/walletStore";
 import { Button } from "@/components/ui/button";
-import { AuthAdapter } from "@web3auth/auth-adapter";
-import {
-  CHAIN_NAMESPACES,
-  UX_MODE,
-  WEB3AUTH_NETWORK,
-} from "@web3auth/base";
-import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
-import { Web3AuthNoModal } from "@web3auth/no-modal";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 export default function Home() {
   const walletStore = useWalletStore();
-  const {address, smartAddress} = useWalletStore();
-  const router = useRouter()
-  
-  const clientId =
-    "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ";
-
-  const chainConfig = {
-    chainNamespace: CHAIN_NAMESPACES.EIP155,
-    chainId: "0x1", // Please use 0x1 for Mainnet
-    rpcTarget: "https://rpc.ankr.com/eth",
-    displayName: "Ethereum Mainnet",
-    blockExplorerUrl: "https://etherscan.io/",
-    ticker: "ETH",
-    tickerName: "Ethereum",
-    logo: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
-  };
-
-  const privateKeyProvider = new EthereumPrivateKeyProvider({
-    config: { chainConfig },
-  });
-
-  const web3authInstance = new Web3AuthNoModal({
-    clientId,
-    web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
-    privateKeyProvider,
-  });
-
-  const authAdapter = new AuthAdapter({
-    adapterSettings: {
-      uxMode: UX_MODE.REDIRECT,
-      loginConfig: {
-        google: {
-          verifier: "w3ads",
-          typeOfLogin: "google",
-          clientId:
-            "519228911939-snh959gvvmjieoo4j14kkaancbkjp34r.apps.googleusercontent.com", //use your app client id you got from google
-        },
-      },
-    },
-  });
-  web3authInstance.configureAdapter(authAdapter);
+  const { address, smartAddress } = useWalletStore();
+  const router = useRouter();
 
   useEffect(() => {
     const initialize = async () => {
       await walletStore.init();
-      console.log("done init", address)
-    }
+      console.log("done init", address);
+    };
     initialize();
   }, []);
 
@@ -71,11 +24,18 @@ export default function Home() {
     await walletStore.login();
   };
 
+  const loginWithWorldID = async () => {
+    if (address) {
+      return;
+    }
+    await walletStore.loginWithWorldID();
+  };
+
   return (
     <>
       <h1>Hello, Home page!</h1>
       <p>
-        { !!address ? (
+        {!!address ? (
           <>
             Logged in as EOA:
             <a
@@ -106,13 +66,30 @@ export default function Home() {
             </Button>
           </>
         ) : (
-          <Button onClick={() => login()}>Login</Button>
+          <>
+            <Button onClick={() => login()}>Login with Google</Button>
+            <Button onClick={() => loginWithWorldID()}>
+              Login with WorldID
+            </Button>
+          </>
         )}
       </p>
 
       <hr></hr>
-      <Button onClick={() => {router.push("/wallet")}}>User Wallet</Button>
-      <Button onClick={() => {router.push("/dashboard")}}>Dashboard</Button>
+      <Button
+        onClick={() => {
+          router.push("/wallet");
+        }}
+      >
+        User Wallet
+      </Button>
+      <Button
+        onClick={() => {
+          router.push("/dashboard");
+        }}
+      >
+        Dashboard
+      </Button>
     </>
   );
 }
--- src/components/stores/walletStore.ts
+++ src/components/stores/walletStore.ts
@@ -1,15 +1,21 @@
-'use client'
-import { createNexusClient, NexusClient } from '@biconomy/sdk';
-import { AuthAdapter } from '@web3auth/auth-adapter';
-import { CHAIN_NAMESPACES, IProvider, UX_MODE, WALLET_ADAPTERS, WEB3AUTH_NETWORK } from '@web3auth/base'
-import { EthereumPrivateKeyProvider } from '@web3auth/ethereum-provider';
-import { Web3AuthNoModal } from '@web3auth/no-modal';
-import { Hex, http } from 'viem';
-import { privateKeyToAccount } from 'viem/accounts';
-import { baseSepolia } from 'viem/chains';
-import { create } from 'zustand'
+"use client";
+import { createNexusClient, NexusClient } from "@biconomy/sdk";
+import { AuthAdapter } from "@web3auth/auth-adapter";
+import {
+  CHAIN_NAMESPACES,
+  IProvider,
+  UX_MODE,
+  WALLET_ADAPTERS,
+  WEB3AUTH_NETWORK,
+} from "@web3auth/base";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+import { Web3AuthNoModal } from "@web3auth/no-modal";
+import { Hex, http } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { baseSepolia } from "viem/chains";
+import { create } from "zustand";
 const clientId =
-"BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ";
+  "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ";
 
 const chainConfig = {
   chainNamespace: CHAIN_NAMESPACES.EIP155,
@@ -42,27 +48,33 @@ const authAdapter = new AuthAdapter({
         clientId:
           "519228911939-snh959gvvmjieoo4j14kkaancbkjp34r.apps.googleusercontent.com", //use your app client id you got from google
       },
+      jwt: {
+        verifier: "w3a-worldcoin-demo",
+        typeOfLogin: "jwt",
+        clientId: "7u5jfJ3bakrfLBJYhyTquohpOth0Tmt7",
+      },
     },
   },
 });
 
-
 export interface WalletState {
-  provider: IProvider| null;
+  provider: IProvider | null;
   address: Hex | undefined;
   smartAddress: Hex | undefined;
   nexusClient: NexusClient | null;
   init: () => Promise<void>;
   login: () => Promise<void>;
+  loginWithWorldID: () => Promise<void>;
   logout: () => Promise<void>;
-  setProvider : ( provider: IProvider| null) => Promise<void>;
+  setProvider: (provider: IProvider | null) => Promise<void>;
   setNexusClient: (nexusClient: NexusClient | null) => void;
   removeProvider: () => void;
 }
-  
-const bundlerUrl = "https://bundler.biconomy.io/api/v3/84532/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44";
+
+const bundlerUrl =
+  "https://bundler.biconomy.io/api/v3/84532/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44";
 export const useWalletStore = create<WalletState>((set, get) => ({
-  provider : null,
+  provider: null,
   address: undefined,
   smartAddress: undefined,
   nexusClient: null,
@@ -72,7 +84,7 @@ export const useWalletStore = create<WalletState>((set, get) => ({
     web3authInstance.configureAdapter(authAdapter);
     if (state.address) return;
     await web3authInstance.init();
-    console.log(web3authInstance.connected)
+    console.log(web3authInstance.connected);
     if (web3authInstance.connected) {
       const address = await web3authInstance.provider?.request({
         method: "eth_accounts",
@@ -83,31 +95,31 @@ export const useWalletStore = create<WalletState>((set, get) => ({
       const privateKey = await web3authInstance.provider?.request({
         method: "eth_private_key",
       });
-      const account = privateKeyToAccount(`0x${privateKey}`)
-      const nexusClient = await createNexusClient({ 
-          signer: account, 
-          chain: baseSepolia,
-          transport: http(), 
-          bundlerTransport: http(bundlerUrl), 
+      const account = privateKeyToAccount(`0x${privateKey}`);
+      const nexusClient = await createNexusClient({
+        signer: account,
+        chain: baseSepolia,
+        transport: http(),
+        bundlerTransport: http(bundlerUrl),
       });
       const smartAccountAddress = nexusClient.account.address;
-      
+
       console.log("connected");
 
-      const {provider} = web3authInstance;
+      const { provider } = web3authInstance;
       const localAddress = await provider?.request<undefined, Hex[]>({
         method: "eth_accounts",
       });
-      console.log(localAddress)
+      console.log(localAddress);
       set(() => ({
-          provider: provider,
-          address: localAddress?.at(0),
-          smartAddress: smartAccountAddress,
-          nexusClient: nexusClient,
-      }))
+        provider: provider,
+        address: localAddress?.at(0),
+        smartAddress: smartAccountAddress,
+        nexusClient: nexusClient,
+      }));
     }
   },
-  login : async () => {
+  login: async () => {
     const state = get();
     if (state.address) {
       return;
@@ -123,26 +135,48 @@ export const useWalletStore = create<WalletState>((set, get) => ({
     set(() => ({
       provider: provider,
       address: localAddress?.at(0),
-    })) 
+    }));
   },
-  logout: async () =>{
+  loginWithWorldID: async () => {
+    const state = get();
+    if (state.address) {
+      return;
+    }
+
+    const provider = await web3authInstance.connectTo(WALLET_ADAPTERS.AUTH, {
+      loginProvider: "jwt",
+      extraLoginOptions: {
+        domain: "https://web3auth.jp.auth0.com",
+        verifierIdField: "sub",
+        connection: "worldcoin",
+      },
+    });
+    const localAddress = await provider?.request<undefined, Hex[]>({
+      method: "eth_accounts",
+    });
+
+    set(() => ({
+      provider: provider,
+      address: localAddress?.at(0),
+    }));
+  },
+  logout: async () => {
     await web3authInstance.logout();
     set(() => ({
       provider: undefined,
       address: undefined,
-    }))  
+    }));
   },
-  setProvider: async ( provider ) =>  {
+  setProvider: async (provider) => {
     const localAddress = await provider?.request<undefined, Hex[]>({
       method: "eth_accounts",
     });
-    
 
     set(() => ({
-        provider: provider,
-        address: localAddress?.at(0),
-    }))
+      provider: provider,
+      address: localAddress?.at(0),
+    }));
   },
   removeProvider: () => set({ provider: null }),
   setNexusClient: (nexusClient: NexusClient | null) => set({ nexusClient }),
-}))
+}));
