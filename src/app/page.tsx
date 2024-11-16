7b39f07cf3fe16f3ed18df2f685eb85f87ec5453
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2024-11-17 00:57:43 +0700

    fix hydration issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,12 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const walletStore = useWalletStore();
   const { address } = useWalletStore();
+  const [loggedIn, setLoggedIn] = useState(false);
   const router = useRouter();
 
   const login = async () => {
@@ -29,18 +30,20 @@ export default function Home() {
       return;
     }
     await walletStore.loginWithWorldID();
+    setLoggedIn(true);
   };
 
   useEffect(() => {
     if (walletStore.web3authInstance?.status === "connected") {
+      setLoggedIn(true);
       router.push("/wallet");
     }
-  }, []);
+  }, [walletStore.web3authInstance?.status, router]);
 
   return (
     <>
       <div>
-        {!!address && walletStore.web3authInstance?.status === "connected" ? (
+        {loggedIn ? (
           <div className="flex h-screen items-center justify-center">
             <div className="flex flex-col items-center gap-4">
               <div className="h-12 w-12 animate-spin rounded-full border-4 border-primary border-t-transparent" />
@@ -69,19 +72,25 @@ export default function Home() {
                     </Button>
                     <Button
                       className="rounded-full w-full"
-                      onClick={() => loginWithWorldID()}
+                      onClick={() => {
+                        loginWithWorldID();
+                        window.open("https://simulator.worldcoin.org/");
+                      }}
                     >
                       Login with WorldID
                     </Button>
-                    <a
+                    {/* <a
                       className="text-center text-sm text-blue-500 underline"
                       href="https://simulator.worldcoin.org/"
                       target="_blank"
                     >
                       WorldID Simulator
-                    </a>
+                    </a> */}
                     <div className="text-xs text-gray-500 text-center mt-6">
-                      Powered by Web3Ads
+                      Powered by{" "}
+                      <a href="https://w3ads.vercel.app" target="_blank">
+                        Web3Ads
+                      </a>
                     </div>
                   </CardContent>
                 </Card>
