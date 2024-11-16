e5043a69fdf047b337d41ea11684adbf05134fd0
Tai Nguyen <tai.nguyen@tor.us>
2024-11-16 22:36:47 +0700

    fix issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useWalletStore } from "@/components/stores/walletStore";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
